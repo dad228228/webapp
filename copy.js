31512a47d3f4273315e4d6fe554bf5ec2945c835
dad228228 <asdda.17@mail.ru>
2024-04-28 00:48:49 +0500

    change

--- copy.js
+++ copy.js
@@ -3,14 +3,15 @@ const textToCopy = refLinkBtn.href
 refLinkBtn.addEventListener('click', (event) => {
   event.preventDefault();
 
-  if (navigator.clipboard) {
-    navigator.clipboard.writeText(textToCopy)
-  } else {
-    const textArea = document.createElement('textarea');
-    textArea.value = textToCopy;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
-  }
+  const textArea = document.createElement('textarea');
+  textArea.value = textToCopy;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+
 })
+
+//if (navigator.clipboard) {
+    //navigator.clipboard.writeText(textToCopy)
+  //} else {
