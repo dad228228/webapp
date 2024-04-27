69892eba9073f725cb9ac59d31683bf4f7dd8ee4
dad228228 <asdda.17@mail.ru>
2024-04-28 01:18:56 +0500

    change

--- copy.js
+++ copy.js
@@ -3,16 +3,12 @@ const textToCopy = refLinkBtn.href;
 refLinkBtn.addEventListener('click', (event) => {
   event.preventDefault();
 
-  if (navigator.clipboard) {
-    navigator.clipboard.writeText(textToCopy);
-  } else {
-    const textArea = document.createElement('textarea');
-    textArea.value = textToCopy;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
-  }
+  const textArea = document.createElement('textarea'); // Для управления буфером в telegram mini apps работает только такой способ
+  textArea.value = textToCopy;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
 })
 
 //if (navigator.clipboard) {
