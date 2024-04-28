823b0e0abd6dac75969469433ba6c4b8bfb17321
dad228228 <asdda.17@mail.ru>
2024-04-28 16:47:54 +0500

    change

--- anim.js
+++ anim.js
@@ -1,18 +1,19 @@
-window.onload = function() {
-  var anchors = document.getElementsByClassName('operation');
-  console.log(anchors)
-  for (var i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener('active', function() {
-      addanim(this);
+const btns = document.querySelectorAll('.operation');
+
+btns.forEach(el => {
+    el.addEventListener('click', function(e) {
+        let
+            size = Math.max(this.offsetWidth, this.offsetHeight),
+            x = e.offsetX - size / 2,
+            y = e.offsetY - size / 2,
+            wave = this.querySelector('.wave')
+
+        // Create an element if it doesn't exist
+        if (!wave) {
+            wave = document.createElement('div')
+            wave.className = 'wave'
+        }
+        wave.style.cssText = `width:${size}px;height:${size}px;top:${y}px;left:${x}px`
+        this.appendChild(wave)
     });
-  }
-
-  function addanim(el) {
-    console.log(el)
-    el.classList.add('active');
-  }
-
-  function endanim(el) {
-    el.classList.remove('active');
-  }
-}
+})
